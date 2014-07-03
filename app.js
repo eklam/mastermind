762bbedc8446a4a39fafa50f36791c704b434973
rmalke <malke.eklam@gmail.com>
2014-07-03 08:42:47 -0300

    Fixing procfile

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +14,8 @@ var session = require('express-session')
 var app = express();
 
     // view engine setup
+app.set('port', process.env.PORT || 3000);
+    
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -66,3 +69,7 @@ app.use(function (err, req, res, next) {
 
 
 module.exports = app;
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
